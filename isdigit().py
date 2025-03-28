6c1fd2e14992244614da9fba471ea8ebe1ff3b34
epha29 <barechyenatu@gmail.com>
2025-03-28 03:22:33 -0700

    isdigit().py

--- isdigit().py
+++
@@ -1,5 +0,0 @@
-text="2929"
-print(text.isdigit())# we expect true becuase all the character are digits
-text2="effa2929"
-print(text2.isdigit())# we expect false because there is a character in the string 
-
